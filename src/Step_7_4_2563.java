fa29014a9a08ac4f778968b7087918ae534181c2
lmg629 <lmg629@naver.com>
2025-10-21 15:21:45 +0900

    Step-7

---
+++ src/Step_7_4_2563.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Step_7_4_2563 {
+
+
+    public static void main(String[] args) {
+        boolean[][] drawing_paper = new boolean[100][100];
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int area = 0;
+        for (int i = 0; i < number; i++) {
+
+            int y = sc.nextInt();
+            int x = sc.nextInt();
+
+            for (int k = y; k < y + 10; k++) {
+                for (int l = x; l < x + 10; l++) {
+                    drawing_paper[k][l] = true;
+                }
+            }
+        }
+        for (int n = 0; n < 100; n++) {
+            for (int m = 0; m < 100; m++) {
+                if (drawing_paper[n][m] == true) {
+                    area = area + 1;
+                }
+            }
+        }
+        System.out.println(area);
+    }
+}
