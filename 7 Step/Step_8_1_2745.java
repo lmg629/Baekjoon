5517dfdd4eac870050aa0a751d92e1f7732a5b6b
lmg629 <lmg629@naver.com>
2025-10-22 10:19:04 +0900

    Step-8

---
+++ 7 Step/Step_8_1_2745.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Step_8_1_2745 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number_N = sc.next();
+        int number_B = sc.nextInt();
+
+        System.out.println(Integer.parseInt(number_N,number_B));
+
+    }
+}
