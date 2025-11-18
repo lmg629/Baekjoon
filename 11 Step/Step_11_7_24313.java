da5b8bf1fab80e6dddb0568e811d08f9fa12020e
lmg629 <lmg629@naver.com>
2025-11-18 11:35:49 +0900

    Step-11

---
+++ 11 Step/Step_11_7_24313.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Step_11_7_24313 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a0 = sc.nextInt();
+        int c = sc.nextInt();
+        int n0 = sc.nextInt();
+        if(a1 * n0 + a0 <= c * n0 && a1 <= c){
+            System.out.println("1");
+            return;
+        }
+        System.out.println("0");
+
+    }
+}
