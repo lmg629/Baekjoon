61ef0289d57a41bdb146dc2a1cac555fa2e3e11f
lmg629 <lmg629@naver.com>
2025-11-25 16:29:14 +0900

    Step-12

---
+++ 12 Step/Step_12_3_19532.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Step_12_3_19532 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        int x = (c*e - b*f) / (a*e - b*d);
+        int y = (c*d - a*f) / (b*d - a*e);
+        System.out.println(x + " "+ y);
+
+    }
+}
+
